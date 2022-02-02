bc5bdf97363640e7b7a088011ed4a060b851471d
gaurang2200 <guptakgk14@gmail.com>
2022-02-02 15:40:15 +0530

    Restructuring Done

--- contracts/FanClubNFT.sol
+++ contracts/FanClubNFT.sol
@@ -5,151 +5,58 @@ import "hardhat/console.sol";
 
 // import libraries for NFT implementation
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "hardhat/console.sol";
+
 
 contract FanClubNFT is ERC1155, Ownable {
-    // artistId = address of the artist
-    struct ArtistAttributes {
-        address artistId;
-        string artistName;
-        string artistImageURI;
-        string artCategory;
-    }
-
-    // An array to store all artists onboarded
-    /* Q. How to check if an artist is already present in this array of not ?? */
-    ArtistAttributes[] private artists;
-
+    // To set up the Name of the collection and Symbol of the Token
+    string public name;
+    string public symbol;
+    
+    // Counter for all the NFT Ids
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     struct TokenInfo {
-        string tokenURI;
-        uint32 count;
+        uint tokenId;
         uint32 totalCount;
         uint256 tokenPrice;
     }
 
-    // A mapping of tokenIds to tokenInfo 
-    mapping(uint256 => TokenInfo) private tokenIdToTokenInfo;
-
-    // A mapping of artistId to tokenID
-    mapping(address => uint256[]) public artistIdToTokenId;
-
-    // A mapping of artistId to arrayIndex in artists
-    mapping(address => uint256) private addressToIndex;
-
-
-    /* Q. Is this necessary ??
-     Because there is no need of tokenId on frontend */
-    event FanClubNFTMinted(address sender, uint256 tokenId);
     
-
     constructor() ERC1155("") {
         console.log("This is Fan Club NFT contract. Heck yeahh !! ");
-        _tokenIds.increment();
-    }
-
-    function getAllArtists() public view returns (ArtistAttributes[] memory) {
-        return artists;
-    }
-
-    /**
-        This function is supposed to be called when any artist registers themselves
-        Checks:
-        -   Return if the Artist is already present
-        -   Set the AddressToIndex mapping
-    **/
-    function addArtist(
-        address _artistAddress,
-        string memory _artistName,
-        string memory _imageURI,
-        string memory _artCategory
-    ) public {
-        require(addressToIndex[_artistAddress] == 0, "Artist Already Registered");
-
-        artists.push(
-            ArtistAttributes({
-                artistId: _artistAddress,
-                artistName: _artistName,
-                artistImageURI: _imageURI,
-                artCategory: _artCategory
-            })
-        );
-
-        // Every Index would be greater than equal to 1
-        addressToIndex[_artistAddress] = artists.length;
-
-        console.log("Artist added");
     }
     
-    /**
-        This function mints an NFT and update the state mappings
-        Checks:
-        -   Allow minting if the artist is a registered Artist
-    **/
+
     function launchNFT(
         uint32 _amount,
-        uint256 _price,
+        uint256 _tokenPrice,
         string memory _tokenURI
-    ) public {
-        // Checking Artist is registered or not
-        require(addressToIndex[msg.sender] > 0 , "This Artist-Address is not Registered");
+    ) public returns (TokenInfo memory) {
+        
+        _tokenIds.increment();
+        uint256 tokenId = _tokenIds.current();
 
-        uint tokenId = _tokenIds.current();
-
-        // Setting mapping artistId to tokenID
-        artistIdToTokenId[msg.sender].push(tokenId);
-
-        // Setting mapping tokenId to tokenInfo
-        tokenIdToTokenInfo[tokenId] = TokenInfo({
-            tokenURI: _tokenURI,
-            count: 0,
-            totalCount: _amount,
-            tokenPrice: _price
-        });
+        setTokenURI(tokenId, _tokenURI);
 
         // Minting the NFT
         _mint(msg.sender, tokenId, _amount, "");
 
-        // Increment the _tokenIds
-        _tokenIds.increment();
-    }
+        TokenInfo memory newToken = TokenInfo(
+            tokenId,
+            _amount,
+            _tokenPrice
+        );
 
-    // Q. What to do here? 😅️
-    function uri(uint256 _tokenId) override public view returns(string memory){
-        
+        return newToken;
     }
+    
 
     function setTokenURI(uint tokenId, string memory _newURI) public onlyOwner {
 
-    }
-
-    // Returns info of an specific artist
-    function getArtistInfo(address _artistAddress) public view returns(ArtistAttributes memory){
-        return artists[addressToIndex[_artistAddress] - 1];
-    }
-
-    // Getting all NFTs minted by a specific Artist
-    function getNFTsOfArtist(address _artistAddress) public view returns(TokenInfo[] memory){
-        uint tokenId = 0;
-        uint256 arrayLength = artistIdToTokenId[_artistAddress].length;
-
-        TokenInfo[] memory artistAllNFTs = new TokenInfo[](arrayLength);
-        
-        for(uint32 i = 0; i < arrayLength; i++) {
-            // Getting each tokenId related to this Artist
-            tokenId = artistIdToTokenId[_artistAddress][i];
-
-            // Getting tokenInfo related to each tokenId
-            artistAllNFTs[i] = tokenIdToTokenInfo[tokenId];
-        }
-
-        return artistAllNFTs;
     }
 
 }
---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,155 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract NFTMarketplace is Ownable, ReentrancyGuard {
+    
+    // Counter for tokenIds
+    // using Counters for Counters.Counter;
+    // Counters.Counter private _tokenIds;
+
+    // Constructor of ERC-1155 sets up the URI to fetch the metadata from that uri
+    constructor() {
+        console.log("This is the NFT Marketplace Contract ^_^");
+    }
+
+    // artistId = address of the artist
+    struct ArtistAttributes {
+        address artistId;
+        string artistName;
+        string artistImageURI;
+        string artCategory;
+    }
+
+    // All the information needed for the token
+    // Here we don't need the Description and Type because that is stored in the metadata on IPFS   
+    struct TokenInfo {
+        uint256 tokenId;
+        uint256 tokenPrice;
+        uint32 count;
+        uint32 totalCount;
+        address currentOwner;
+        address creator;
+    }
+
+    // Emits the event that the NFT is minted and listed
+    event FanClubNFTMinted(
+        uint256 indexed tokenId,
+        address indexed nftContract
+
+    );
+
+    // An array to store all artists onboarded
+    ArtistAttributes[] private artists;
+
+    // Array to store all NFTs
+    TokenInfo[] private nfts;
+
+    // A mapping of tokenIds to tokenInfo
+    // Don't need because we are getting the tokenID from the NFT contract
+    mapping(uint256 => TokenInfo) private tokenIdToTokenInfo;
+
+    // A mapping of artistId to tokenID
+    mapping(address => uint256[]) private artistIdToTokenId;
+
+    // A mapping of artistId to arrayIndex in artists
+    mapping(address => uint256) private addressToIndex;
+
+    // Return all the registered Artists
+    function getAllArtists() public view returns (ArtistAttributes[] memory) {
+        return artists;
+    }
+
+    // Adding New artist to the artists array
+    // Cost of Adding Artist == Gas Price
+    function addArtist(
+        address _artistAddress,
+        string memory _artistName,
+        string memory _imageURI,
+        string memory _artCategory
+    ) public {
+        require(addressToIndex[_artistAddress] == 0, "Artist Already Registered");
+
+        artists.push(
+            ArtistAttributes({
+                artistId: _artistAddress,
+                artistName: _artistName,
+                artistImageURI: _imageURI,
+                artCategory: _artCategory
+            })
+        );
+
+        // Every Index would be greater than equal to 1
+        addressToIndex[_artistAddress] = artists.length;
+
+        console.log("Artist added");
+    }
+    
+    // Listing the NFT
+    function listNFT(
+        address nftContract,
+        address artistAddress,
+        uint256 _tokenId,
+        uint32 _amount,
+        uint256 _tokenPrice
+    ) public nonReentrant {
+        // Checking if the artist is registered or not
+        require(addressToIndex[artistAddress] > 0, "No such Artist Registered");
+
+        // Adding the nft for the specific Artist
+        artistIdToTokenId[artistAddress].push(_tokenId);
+
+        // Adding tokenId mapping to tokenInfo
+        TokenInfo memory newTokenInfo = TokenInfo ({
+            tokenId: _tokenId,
+            count: 0,
+            totalCount: _amount,
+            tokenPrice: _tokenPrice,
+            currentOwner: address(this),
+            creator: artistAddress
+        });
+
+        tokenIdToTokenInfo[_tokenId] = newTokenInfo;
+
+        nfts.push(newTokenInfo);
+
+        IERC1155(nftContract).safeTransferFrom(msg.sender, address(this), _tokenId, _amount, "");
+
+    }
+
+
+    // Returns info of an specific artist
+    function getArtistInfo(address _artistAddress) public view returns (ArtistAttributes memory){
+        return artists[addressToIndex[_artistAddress] - 1];
+    }
+
+    // Getting all NFTs minted by a specific Artist
+    function getNFTsOfArtist(address _artistAddress) public view returns (TokenInfo[] memory){
+        uint tokenId = 0;
+        uint256 arrayLength = artistIdToTokenId[_artistAddress].length;
+
+        TokenInfo[] memory artistAllNFTs = new TokenInfo[](arrayLength);
+        
+        for(uint32 i = 0; i < arrayLength; i++) {
+            // Getting each tokenId related to this Artist
+            tokenId = artistIdToTokenId[_artistAddress][i];
+
+            // Getting tokenInfo related to each tokenId
+            artistAllNFTs[i] = tokenIdToTokenInfo[tokenId];
+        }
+
+        return artistAllNFTs;
+    }
+
+    function getAllNFTs() public view returns (TokenInfo[] memory){
+        return nfts;
+    }
+
+}
