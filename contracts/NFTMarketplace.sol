e1ebac3f8dff6200df9cc23231f2e750cebd79e2
swetshaw <swt.shw94@gmail.com>
2022-02-05 10:25:38 +0530

    Marketplace contract is now ERC1155Holder

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -8,8 +8,9 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 
-contract NFTMarketplace is Ownable, ReentrancyGuard {
+contract NFTMarketplace is Ownable, ReentrancyGuard, ERC1155Holder {
     // Counter for tokenIds
     // using Counters for Counters.Counter;
     // Counters.Counter private _tokenIds;
